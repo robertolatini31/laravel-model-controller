8f363acec49dc72791f6a7ae1a5edad025daaecf
robertolatini31 <robertolatini31@gmail.com>
2022-06-13 14:42:18 +0200

    add controller and model

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Movie;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index() {
+        $movies = Movie::all();
+        dd($movies);
+        return view('welcome', compact($movies));
+    }
+}
